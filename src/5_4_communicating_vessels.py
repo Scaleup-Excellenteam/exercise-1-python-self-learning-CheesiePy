0b4bdc18f14bde4e149da521a31c53e44b4753fd
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2025-04-01 13:52:37 +0300

    finish communicatin_vessels.py and running_2000.py

---
+++ src/5_4_communicating_vessels.py
@@ -0,0 +1,19 @@
+from typing import Iterable, Any, List
+
+def interleave(*args: Iterable[Any]) -> List[Any]:
+    res = []
+    max_len = max(len(arg) for arg in args)
+
+    for i in range(max_len):
+        for j in range(len(args)):
+            res.append(args[j][i])
+    
+    return res
+
+
+
+def main():
+    print(interleave('abc', [1, 2, 3], ('!', '@', '#'))) # ['a', 1, '!', 'b', 2, '@', 'c', 3, '#'])
+
+if __name__=="__main__":
+    main()
---
+++ src/6_2_running_2000.py
@@ -0,0 +1,23 @@
+
+import time 
+def timer(func, *args, **kwargs):
+
+    start = time.time()
+
+    func(*args, **kwargs)
+
+    end = time.time()
+
+    function_name = func.__name__
+
+    elapsed_time = end - start
+    print(f"Function '{function_name}' took {elapsed_time:.6f} seconds to execute with arguments {args or kwargs}.")
+
+
+def main():
+    timer(print, "Hello") #, תחזיר הפונקציה את משך זמן הביצוע של print("Hello").
+    timer(zip, [1, 2, 3], [4, 5, 6])# , תחזיר הפונקציה את משך זמן הביצוע של zip([1, 2, 3], [4, 5, 6]).
+    timer("Hi {name}".format, name="Bug") # , תחזיר הפונקציה את משך זמן הביצוע של "Hi {name}".format(name="Bug")
+
+if __name__=="__main__":
+    main()
