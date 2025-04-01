64b6be95e07594896b11e79e7b4cacbad383d7fb
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2025-04-01 10:57:31 +0300

    done with piece_of_cake.py

--- src/5_2_piece_of_cake.py
+++ src/5_2_piece_of_cake.py
@@ -20,4 +20,25 @@
 """
 
 def get_recipe_price(prices, optionals=None, **kwargs):
-    pass
+    """
+    calc the total cost on the recipe
+    """
+    # price is for 100 grams.. 
+    # if no amout was given then get one unit (100grams)
+    if optionals is None:
+        optionals = []
+    total = 0
+    for ing, amount in kwargs.items():
+        if ing in prices and ing not in optionals:
+            # price is for 100 grams
+            total += (amount / 100) * prices[ing]
+    print(total)
+
+
+def test():
+    get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300)
+    get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100)
+    get_recipe_price({})
+
+if __name__=="__main__":
+    test()
